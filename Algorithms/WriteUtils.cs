6c99916e3a07a20d11788ee1f937e398268c7d43
Tom Howard <thomaswhoward@gmail.com>
2018-03-15 21:48:19 -0400

    QSLomuto console logging

--- Algorithms/WriteUtils.cs
+++ Algorithms/WriteUtils.cs
@@ -5,12 +5,13 @@ namespace Algorithms
 {
     public class WriteUtils
     {
-        public void WriteIntArray(int[] arr) 
+        public static void WriteIntArray(int[] arr) 
         {
             foreach (int item in arr)
             {
                 Write(" {0}", item);
             }
+            WriteLine("");
         }
     }
 }
